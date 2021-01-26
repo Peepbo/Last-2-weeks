04759591068fa687fef1b62ab5db45864898a150
Harriet Lee <lharriet27@gmail.com>
2021-01-26 16:04:02 +0900

    placed npc

---
+++ MainProject/Assets/Scripts/UnitMgr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMgr : MonoBehaviour
+{
+    public bool isTalk = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected void Talk()
+    {
+
+    }
+}
