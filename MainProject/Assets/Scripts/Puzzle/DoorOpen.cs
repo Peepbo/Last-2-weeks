4624f0552fe73ea0fd860fad57cc697b8c92f5c3
Harriet Lee <lharriet27@gmail.com>
2021-01-28 19:52:24 +0900

    Add plantaHouse

---
+++ MainProject/Assets/Scripts/Puzzle/DoorOpen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    Transform startPos;
+    Transform endPos;
+
+    void Start()
+    {
+        startPos = transform.GetChild(0).GetComponent<Transform>();
+        endPos = transform.GetChild(0).GetChild(0).GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator FloatObject()
+    {
+        while (true)
+        {
+
+            yield return null;
+
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.name.Equals("FatherPlayer"))
+        {
+            transform.position = endPos.position;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.name.Equals("FatherPlayer"))
+        {
+            transform.position = startPos.position;
+        }
+
+    }
+}
