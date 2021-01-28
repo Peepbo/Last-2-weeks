d1c426f83db1dfafeca4951902e7dbedcd4c9eed
Peepbo <tngus10778@gmail.com>
2021-01-28 19:49:25 +0900

    Particle Asset, home, outdoor (night version)

---
+++ MainProject/Assets/DoorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+
+
+    [Space]
+    public GameObject buttonHead;
+    Animator btnAnim;
+
+    private void Awake()
+    {
+        btnAnim = buttonHead.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player")) buttonHead.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag.Equals("Player")) btnAnim.SetTrigger("Out");
+    }
+}
