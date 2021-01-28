dbded91310f75cd465a596663a9f3ad6c3bc775c
Peepbo <tngus10778@gmail.com>
2021-01-28 13:40:01 +0900

    Add console button Image (always look camera)

---
+++ MainProject/Assets/AlwaysLookCam.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysLookCam : MonoBehaviour
+{
+    Transform transformCam;
+    private void Start() => transformCam = Camera.main.transform;
+    private void Update() => transform.forward = transformCam.forward;
+}
