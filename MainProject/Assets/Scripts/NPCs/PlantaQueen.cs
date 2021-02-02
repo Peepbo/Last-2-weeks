ca8d7a61fe073aac63257361ee765ee5d0dc82bf
Harriet Lee <lharriet27@gmail.com>
2021-02-02 19:07:26 +0900

    set places of planta quuen

--- MainProject/Assets/Scripts/NPCs/PlantaQueen.cs
+++ MainProject/Assets/Scripts/NPCs/PlantaQueen.cs
@@ -4,22 +4,59 @@ using UnityEngine;
 
 public class PlantaQueen : UnitMgr
 {
+    public GameObject flower;
+    private Vector3 rightPlace;
+    public bool isActive=false;
+    public enum WHERE
+    {
+        TOWN, PLANTA_HOUSE, PUZZLE
 
+    }
+
+    public WHERE place;
     protected override void Awake()
     {
         base.Awake();
+        rightPlace = flower.transform.position;
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    protected override void Update()
+    private void Update()
     {
-        base.Update();
+        
+        switch (place)
+        {
+            case WHERE.TOWN:
+                {
+                    if (isCol)
+                    {
+                        Stare();
+                    }
+                    else transform.rotation = startRotation;
+                }
+               
+                break;
+            case WHERE.PLANTA_HOUSE:
+                {
+                    if(isCol)
+                    {
+                        MoveFlower();
+                    }
+                }
+                break;
+            case WHERE.PUZZLE:
+                break;
+        }
+
     }
 
-    
 
+    public void MoveFlower()
+    {
+        
+    }
 }
